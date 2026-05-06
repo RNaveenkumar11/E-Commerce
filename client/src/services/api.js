1f7734a7c513142e916f57ae5c7f56b1c34cf511
Naveenkumar R <107539834+RNaveenkumar11@users.noreply.github.com>
2026-05-06 14:16:53 +0530

    Update API base URL to include '/api' path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-commerce-backend-lasw.onrender.com",
+  baseURL: "https://e-commerce-backend-lasw.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
