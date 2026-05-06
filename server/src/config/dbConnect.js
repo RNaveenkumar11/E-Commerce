d008b01bc9ba3821e5bd3886f9476134ef3e471b
Naveenkumar R <107539834+RNaveenkumar11@users.noreply.github.com>
2026-05-06 14:03:38 +0530

    Update MongoDB connection string to use environment variable

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDb = async() =>{
         try{
-            await mongoose.connect("mongodb://localhost:27017/auth")
+            await mongoose.connect(process.env.MONGO_URL)
             console.log("Connected to DB")
         }catch{
              console.log("Error connecting to DB")
